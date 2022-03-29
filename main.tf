88d72f871515d1ec646ab462151ae243aac302bb
grant <grantpage.net>
2022-03-28 23:32:04 -0500

    Try and setup a lightsail instance as part of the terraform-powered CI-CD.

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  default = "default"
+  region = "us-east-2"
+}
+
+resource "aws_lightsail_instance" "nodejs_terraform" {
+  name              = "nodejs_terraform"
+  availability_zone = "us-east-2a"
+  blueprint_id      = "nodejs"
+  bundle_id         = "micro_2_0"
+  user_data         = "npm start -i"
+}
